8f591e5bde362e8711428b2af1707c568bea7ef9
Nick FeD <sokolovandrian88@gmail.com>
2023-06-22 14:41:16 +0300

    Добавлен DeskController

---
+++ TaskManager.Api/Controllers/DeskController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Api.Data;
+using TaskManager.Api.Models.Abstracted;
+using TaskManager.Api.Services;
+using TaskManager.Command.Models;
+
+namespace TaskManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeskController : CRUDController<DeskModel, DeskService>
+    {
+        public DeskController(ApplicationContext context) : base(new(context))
+        {
+        }
+    }
+}
