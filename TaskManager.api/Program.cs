e18bb2e0f6c58f5118c9bae536fe7dccd4121557
NickFeD <66272332+NickFeD@users.noreply.github.com>
2023-07-21 18:06:20 +0300

    убрал лишние using в program

--- TaskManager.api/Program.cs
+++ TaskManager.api/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.ComponentModel;
 namespace TaskManager.Api
 {
     public class Program
